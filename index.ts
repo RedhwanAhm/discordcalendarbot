246fd1e80027ea24b79d2af313bb3d1136786ec0
RedhwanAhm <redhwan.ah1202@gmail.com>
2022-07-14 18:29:26 -0700

    testing out discordBot

--- index.ts
+++ index.ts
@@ -1,2 +1,29 @@
-import DiscordJS from 'discord.js'
+import DiscordJS, { Intents } from 'discord.js'
 import dotenv from 'dotenv'
+
+const { google } = require('googleapis');
+
+const { OAuth2 } = google.auth;
+
+dotenv.config()
+
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+
+client.on('ready', () => {
+    console.log("the bot is ready");
+})
+
+client.on('messageCreate', (message) => {
+    if(message.content === 'ping') {
+        message.reply({
+            content: 'pong'
+        })
+    }
+})
+
+client.login(process.env.TOKEN);
